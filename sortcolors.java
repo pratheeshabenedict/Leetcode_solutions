5b8b2be7c8594628bd676f8a9cc600e4b180af9f
Pratheesha B <pratheeshabenedict@gmail.com>
2025-02-26 15:15:21 +0530

    Create sortcolors.java

---
+++ sortcolors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int start=0;
+        int end = nums.length-1;
+        int mid = 0;
+        while(mid<=end){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[start];
+                nums[start]=temp;
+                start++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                int temp=nums[mid];
+                nums[mid]=nums[end];
+                nums[end]=temp;
+                end--;
+            }
+        }
+    }
+}
